fadd3c875fcd3758975822a403e3178bb0c482dd
nikryden <nikryden@gmail.com>
2026-03-30 22:34:46 +0200

    feat: add AppVersion.Full with commit hash, show full version in About page

--- LogViewer2026.Core/AppVersion.cs
+++ LogViewer2026.Core/AppVersion.cs
@@ -4,9 +4,14 @@ namespace LogViewer2026.Core;
 
 public static class AppVersion
 {
-    public static string Current { get; } =
+    /// <summary>Full version including commit hash, e.g. "0.6.0-alpha1+abc1234".</summary>
+    public static string Full { get; } =
         typeof(AppVersion).Assembly
             .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
             ?.InformationalVersion
         ?? "0.0.0";
+
+    /// <summary>Short version without commit hash, e.g. "0.6.0-alpha1".</summary>
+    public static string Current { get; } =
+        Full.Contains('+') ? Full[..Full.IndexOf('+')] : Full;
 }
